40d882fd36b3fa6713cb3182e41f0090fd5ef5f6
Yash-2907 <guptayp_1@rknec.edu>
2025-03-25 01:23:18 +0530

    UI setup and Routing pages with Authentication done

--- components/header.jsx
+++ components/header.jsx
@@ -1,16 +1,41 @@
-import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton} from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
+import UserMenu from "./user-menu";
 
 const Header = () => {
-    return(
-        <div>
-            <SignedOut>
-                <SignInButton/>
-            </SignedOut>
-            <SignedIn>
-                <UserButton/>
-            </SignedIn>
-        </div>
-    ) 
+    return (
+        <header className="container mx-auto">
+            <nav className="py-6 px-4 flex justify-between items-center">
+                <Link href="/">
+                    <Image src="/scrumLogo.png" alt="Scrum Logo" width={200} height={200}
+                        className="h-10 w-auto object-contain" />
+                </Link>
+                <div className="flex items-center gap-4">
+                    <Link href='/project/create'>
+                    <Button variant='destructive' className="flex items-center gap-2">
+                        <PenBox size={18}></PenBox>
+                        <span>
+                            Create Project
+                        </span>
+                    </Button>
+                    </Link>
+                    <SignedOut>
+                        <SignInButton forceRedirectUrl="/onboarding">
+                        <Button variant="outline">
+                          Login
+                        </Button>
+                        </SignInButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserMenu/>
+                    </SignedIn>
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
---
+++ components/user-menu.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+import { ChartNoAxesGantt } from "lucide-react";
+import React from "react";
+
+const UserMenu = () => {
+    return <UserButton appearance={{
+        elements : {
+            avatarBox : "w-10 h-10",
+        },
+    }}>
+        <UserButton.MenuItems>
+            <UserButton.Link
+            label="My Organization"
+            labelIcon={<ChartNoAxesGantt size={15} />}
+            href="/onboarding"
+            />
+            <UserButton.Action label="manageAccount"/>
+        </UserButton.MenuItems>
+    </UserButton>;
+};
+
+export default UserMenu;
