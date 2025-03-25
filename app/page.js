79ac65066417cd276197026222c9ff11b6b074db
Yash-2907 <guptayp_1@rknec.edu>
2025-03-25 17:07:40 +0530

    front page 3

--- app/page.js
+++ app/page.js
@@ -1,7 +1,31 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import {ChevronRight} from "lucide-react";
+import {ArrowRight, BarChart,Calender,ChevronRight,Layout} from "lucide-react";
+
+import faqs from '@/data/faqs'
 import Link from "next/link";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
+const features=[
+  {
+      title:"Intuitive Kanban Boards",
+      description:
+      "Visualze your workflow and optimize team productivity with our easy-to-use kanban boards.",
+      icon:Layout,
+  },
+  {
+      title:"Powerful Sprint Planning",
+      description:
+      "Plan and manage sprints effectively,ensuring your team stays focused on delivering value.",
+      icon:Calender,
+  },
+  {
+      title:"Comprehensive Reporting",
+      description:
+      "Gaininsight into your team's performance with detailed,customizable reports and analytics.",
+      icon:BarChart,
+  },
+];
 
 export default function Home() {
   return <div className="min-h-screen">
@@ -37,7 +61,56 @@ offering a smooth and interactive experience. The site prioritizes speed, securi
 </Button>
 
 </Link>
-</section>
+{/* <section id="features " className="bg-gray-900 py-20 px-5">
+            <div className="container mx-auto">
+              <h3 className="text-3xl font-bold mb-12 text-center">Key Features </h3> 
+              <div className="grid grid-clos-1 lg:grid-cols-3 gap-8">{features.map((feature,index) =>{
+                return(
+                    <Card key={index} className="bg-gray-800">
+                 <CardContent className='pt6'>
+                 <feature.icon className="h-12 w-12 mb-4 text-blue-300"/>
+                 <h4 className="text-xl font-semibold mb-2"> {feature.title}    </h4>
+                 <p className="text-gray-300">{feature.description}</p>
+                    </CardContent>
+                 </Card>
 
-</div>
+                );
+            })
+              }        
+              </div> 
+            </div>
+        </section> */}
+<section className="bg-gray-900 py-20 px-5"> 
+            <div className="container mx-auto">
+                <h3 className="text-3xl font-bold mb-12 text-center">
+                Frequently Asked Question
+                </h3>
+                <Accordion type="single" collapsibl className="W-full">
+                    {faqs.map((faq,index)=>(
+                <AccordionItem key={index} value={'item-${index}'}>
+                <AccordionTrigger>{faqs.question}</AccordionTrigger>
+                <AccordionContent>
+                {faqs.answer}
+               </AccordionContent>
+               </AccordionItem>))}
+               </Accordion>
+
+
+            </div>
+        </section>
+</section>
+<section className=" py-20 text-center px-5 "> 
+            <div className="container mx-auto">
+                <h3 className="text-3xl font-bold mb-5 text-center">
+              Ready to Transform Your Workflow
+                </h3>
+                <p className="text-xl-mb-12">Join our team for new and better experiment.</p>
+                <Link href='onboarding'>
+              <Button size='lg' className="animate-bounce"> Start For free <ArrowRight className="ml-2 h-5 w-5"/>
+                </Button>  
+                </Link>
+            </div>
+        </section>
+    </div>;
 }
+
