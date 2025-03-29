255dbe358c779a2ac1e5c145d168d505b4662f5e
Rutuja Mane <manerd@rknec.edu>
2025-03-29 14:06:53 +0530

    Initial commit 🚀

--- components/header.jsx
+++ components/header.jsx
@@ -4,8 +4,11 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import { PenBox } from "lucide-react";
 import UserMenu from "./user-menu";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+
+const Header =async () => {
+        await checkUser();
     return (
         <header className="container mx-auto">
             <nav className="py-6 px-4 flex justify-between items-center">
