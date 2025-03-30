26f92ed1c9ef15543ed7a6aac4ef653d3db85d3a
Yash-2907 <guptayp_1@rknec.edu>
2025-03-31 02:20:46 +0530

    sprint add and create functionality added

--- app/(main)/project/[projectId]/page.jsx
+++ app/(main)/project/[projectId]/page.jsx
@@ -1,42 +1,34 @@
-import react from 'react';
+import { getProject } from "@/actions/projects";
+import { notFound } from "next/navigation";
+import SprintCreationForm from "../_components/create-sprint";
+import SprintBoard from "../_components/sprint-board";
 
-const ProjectPage = () => {
-    return <div>Project page</div>
-};
+export default async function ProjectPage({ params }) {
+  const { projectId } = params;
+  const project = await getProject(projectId);
 
-export default ProjectPage;
+  if (!project) {
+    notFound();
+  }
 
-// import { getProject } from "@/actions/projects";
-// import { notFound } from "next/navigation";
-// import SprintCreationForm from "../_components/create-sprint";
-// import SprintBoard from "../_components/sprint-board";
+  return (
+    <div className="container mx-auto">
+      <SprintCreationForm
+        projectTitle={project.name}
+        projectId={projectId}
+        projectKey={project.key}
+        sprintKey={project.sprints?.length + 1}
+      />
 
-// export default async function ProjectPage({ params }) {
-//   const { projectId } = params;
-//   const project = await getProject(projectId);
-
-//   if (!project) {
-//     notFound();
-//   }
-
-//   return (
-//     <div className="container mx-auto">
-//       <SprintCreationForm
-//         projectTitle={project.name}
-//         projectId={projectId}
-//         projectKey={project.key}
-//         sprintKey={project.sprints?.length + 1}
-//       />
-
-//       {project.sprints.length > 0 ? (
-//         <SprintBoard
-//           sprints={project.sprints}
-//           projectId={projectId}
-//           orgId={project.organizationId}
-//         />
-//       ) : (
-//         <div>Create a Sprint from button above</div>
-//       )}
-//     </div>
-//   );
-// }
+      {project.sprints.length > 0 ? (
+        <SprintBoard
+          sprints={project.sprints}
+          projectId={projectId}
+          orgId={project.organizationId}
+        />
+      ) : (
+        <div>Create a Sprint from button above</div>
+      )}
+    </div>
+  );
+}
